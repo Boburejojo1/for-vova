1b80a41bac0a0dec922275d1aa4e23c9addd78fd
Boburejojo1 <149566656+Boburejojo1@users.noreply.github.com>
2023-12-19 10:50:37 +0200

    Add files via upload

---
+++ не скам/script.js
@@ -0,0 +1,13 @@
+function bobur(){
+ let comf = confirm('нажми що запустити гру')
+ if (comf) {
+    for(let i = 1;i < 12;i--){
+        console.log('ВХХХАХАХА БОООТ')
+    }
+ } else{
+    alert('як так')
+    for(let i = 1;i < 12 ;i--){
+        console.log('треба було погоджуватися')
+ }
+ }
+}
